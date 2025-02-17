ddfde3c5b707d26cbb5f78d3fd8597b7be51194a
lmatiasbelderrain <Matibelde@hotmail.com>
2025-02-17 19:04:20 +0000

    audio player canciones

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [lista, setLista] = useState([])
+	const getSongs = async () => {
+		try {
+			const response = await fetch("https://playground.4geeks.com/sound/songs")
+			console.log(response)
+			const data = await response.json()
+			console.log(data.songs)
+			setLista(data.songs)
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		getSongs()
+	}, [])
+	
+	return (
+		<div className="text-center container">
+			<h1>Sound API</h1>
+			<div className="row">
+				<div className="col-12">
+					{lista.map((cancion) => (
+
+						<button className="btn btn-dark w-75 m-1">{cancion.name}</button>
+					))}
+				</div>
+			</div>
+
+
+
 		</div>
 	);
 };
